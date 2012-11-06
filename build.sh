12f903d8eeb8762b9998b29a0d5a4d85b95ee135
n2j3 <1tsoglani@gmail.com>
2012-11-06 12:49:15 +0200

    include changelog to package

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PLUGIN_NAME=plugin.audio.amazingradio
 
 mkdir $PLUGIN_NAME
-cp *.xml *.py icon.png $PLUGIN_NAME/.
+cp *.txt *.xml *.py icon.png $PLUGIN_NAME/.
 zip -rq $PLUGIN_NAME.zip $PLUGIN_NAME/
 rm -r $PLUGIN_NAME/
